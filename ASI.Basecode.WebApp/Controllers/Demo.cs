431c97ef7e78776e09053a1fc16446a8cc8edf36
CCSLAB526U44\admin44 <dkhrl2000@gmail.com>
2023-10-08 06:46:35 +0800

    DELFIN NEW

---
+++ ASI.Basecode.WebApp/Controllers/Demo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class Demo : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Students()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASI.Basecode.WebApp/Views/Demo/Students.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASI.Basecode.WebApp.Views.Demo
+{
+    public class StudentsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
